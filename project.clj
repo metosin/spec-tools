b152814529d3598f48bf0546cbc5189f1b6a53df
Tommi Reiman <tommi@iki.fi>
2016-08-18 22:03:28 +0300

    Initial setup

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject metosin/spec-tools "0.1.0-SNAPSHOT"
+  :description "Common utilities for clojure.spec"
+  :url "https://github.com/metosin/spec-tools"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
+  :plugins [[funcool/codeina "0.4.0"]]
+
+  :codeina {:target "doc"
+            :src-uri "http://github.com/metosin/spec-tools/blob/master/"
+            :src-uri-prefix "#L"}
+
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]
+                   :dependencies [[criterium "0.4.4"]
+                                  [org.clojure/clojurescript "1.9.216"]]}}
+  :aliases {"all" ["with-profile" "dev"]
+            "test-clj" ["all" "do" ["test"] ["check"]]})
