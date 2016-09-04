500ed35fb5e3447f51d444d56aa7033a2a3e10c7
Tommi Reiman <tommi@iki.fi>
2016-09-04 11:38:32 +0300

    Add Schema to dev def. Perf profile

--- project.clj
+++ project.clj
@@ -14,7 +14,10 @@
 
   :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]]
                    :dependencies [[criterium "0.4.4"]
+                                  [prismatic/schema "1.1.3"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [org.clojure/clojurescript "1.9.227"]]}}
+                                  [org.clojure/clojurescript "1.9.227"]]}
+             :perf {:jvm-opts ^:replace ["-server"]}}
   :aliases {"all" ["with-profile" "dev"]
+            "perf" ["with-profile" "default,dev,perf"]
             "test-clj" ["all" "do" ["test"] ["check"]]})
