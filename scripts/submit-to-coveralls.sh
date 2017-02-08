e9e8a062001c426632e6394a4d6067abfcc800ab
Miikka Koskinen <miikka.koskinen@iki.fi>
2017-02-08 14:52:30 +0200

    Run Cloverage only once per Travis build

--- scripts/submit-to-coveralls.sh
+++ scripts/submit-to-coveralls.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-COVERALLS_URL="https://coveralls.io/api/v1/jobs"
-lein cloverage --coveralls
-curl -F "json_file=@target/coverage/coveralls.json" "$COVERALLS_URL"
+set -euo pipefail
+
+case $1 in
+    cljs)
+        echo "No support for ClojureScript coverage."
+        ;;
+    clj)
+        COVERALLS_URL="https://coveralls.io/api/v1/jobs"
+        lein cloverage --coveralls
+        curl -F "json_file=@target/coverage/coveralls.json" "$COVERALLS_URL"
+        ;;
+    *)
+        echo "Please select [clj|cljs]"
+        exit 1
+        ;;
+esac
+
