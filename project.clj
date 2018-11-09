3187bbcbab04367bbd19572da6af59952ca54a7c
Tommi Reiman <tommi@iki.fi>
2018-11-09 11:10:37 +0200

    Bump up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/spec-tools "0.8.0"
+(defproject metosin/spec-tools "0.8.1"
   :description "Clojure(Script) tools for clojure.spec"
   :url "https://github.com/metosin/spec-tools"
   :license {:name "Eclipse Public License"
