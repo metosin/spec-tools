20ba9d8daa5395987579e35efcee5fbd4887d9a7
Miikka Koskinen <miikka@metosin.fi>
2019-06-07 10:11:25 +0300

    Version 0.9.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/spec-tools "0.9.3"
+(defproject metosin/spec-tools "0.9.4-SNAPSHOT"
   :description "Clojure(Script) tools for clojure.spec"
   :url "https://github.com/metosin/spec-tools"
   :license {:name "Eclipse Public License"
