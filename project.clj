2d9f529fc89942a1e97c5656894937bdfcc8e3a1
Tommi Reiman <tommi@iki.fi>
2023-08-28 13:14:33 +0300

    CHANGELOG + deps

--- project.clj
+++ project.clj
@@ -16,32 +16,33 @@
   :scm {:name "git"
         :url "https://github.com/metosin/spec-tools"}
 
-  :dependencies [[org.clojure/spec.alpha "0.2.187"]]
+  :dependencies [[org.clojure/spec.alpha "0.3.218"]]
 
-  :profiles {:dev {:plugins [[jonase/eastwood "0.3.11"]
-                             [lein-tach "1.0.0"]
+  :profiles {:dev {:plugins [[jonase/eastwood "1.4.0"]
+                             [lein-tach "1.1.0"]
                              [lein-doo "0.1.11"]
                              [lein-cljsbuild "1.1.8"]
-                             [lein-cloverage "1.1.2"]
-                             [lein-codox "0.10.7"]
+                             [lein-cloverage "1.2.4"]
+                             [lein-codox "0.10.8"]
                              [lein-pprint "1.3.2"]]
                    :jvm-opts ^:replace ["-server"]
                    ;:global-vars {*warn-on-reflection* true}
-                   :dependencies [[org.clojure/clojure "1.10.0"]
-                                  [org.clojure/clojurescript "1.10.520"]
-                                  [criterium "0.4.5"]
-                                  [prismatic/schema "1.1.12"]
-                                  [org.clojure/test.check "1.0.0"]
-                                  [org.clojure/tools.namespace "1.0.0"]
-                                  [com.gfredericks/test.chuck "0.2.10"]
+                   :dependencies [[org.clojure/clojure "1.11.1"]
+                                  [org.clojure/clojurescript "1.11.60"]
+                                  [criterium "0.4.6"]
+                                  [prismatic/schema "1.4.1"]
+                                  [org.clojure/test.check "1.1.1"]
+                                  [org.clojure/tools.namespace "1.4.4"]
+                                  [com.gfredericks/test.chuck "0.2.14"]
                                   ; com.bhauman/spell-spec library doesn't get any updates, so it has to be copied here
                                   ; under spec-tools.spell-spec namespace in order to fix its bugs.
                                   ; If the library gets updated with fixes it would be desirable to switch back to it.
                                   ;[com.bhauman/spell-spec "0.1.1"]
-                                  [expound "0.8.4"]
-																																		[metosin/muuntaja "0.6.7"]
+                                  [expound "0.9.0"]
+																																		[metosin/muuntaja "0.6.8"]
                                   [metosin/ring-swagger "0.26.2"]
-                                  [metosin/scjsv "0.6.1"]]}
+                                  [metosin/jsonista "0.3.7"]
+                                  [metosin/scjsv "0.6.2"]]}
              :perf {:jvm-opts ^:replace ["-server"]}}
   :aliases {"all" ["with-profile" "dev"]
             "perf" ["with-profile" "default,dev,perf"]
