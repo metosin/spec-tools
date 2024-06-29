faa304c31dcd67e86edf5f90c1005917c01d95a2
Tommi Reiman <tommi@iki.fi>
2024-06-29 18:16:51 +0300

    0.10.7 for lein too

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/spec-tools "0.10.5"
+(defproject metosin/spec-tools "0.10.7"
   :description "Clojure(Script) tools for clojure.spec"
   :url "https://github.com/metosin/spec-tools"
   :license {:name "Eclipse Public License", :url "https://www.eclipse.org/legal/epl-2.0/"}
