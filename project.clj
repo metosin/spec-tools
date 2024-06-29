24e77507811108eb816f80e74ec0d688313361ac
Tommi Reiman <tommi@iki.fi>
2024-06-29 17:03:37 +0300

    update deps

--- project.clj
+++ project.clj
@@ -16,9 +16,9 @@
   :scm {:name "git"
         :url "https://github.com/metosin/spec-tools"}
 
-  :dependencies [[org.clojure/spec.alpha "0.3.218"]]
+  :dependencies [[org.clojure/spec.alpha "0.5.238"]]
 
-  :profiles {:dev {:plugins [[jonase/eastwood "1.4.0"]
+  :profiles {:dev {:plugins [[jonase/eastwood "1.4.2"]
                              [lein-tach "1.1.0"]
                              [lein-doo "0.1.11"]
                              [lein-cljsbuild "1.1.8"]
@@ -27,21 +27,21 @@
                              [lein-pprint "1.3.2"]]
                    :jvm-opts ^:replace ["-server"]
                    ;:global-vars {*warn-on-reflection* true}
-                   :dependencies [[org.clojure/clojure "1.11.1"]
-                                  [org.clojure/clojurescript "1.11.60"]
+                   :dependencies [[org.clojure/clojure "1.11.3"]
+                                  [org.clojure/clojurescript "1.11.132"]
                                   [criterium "0.4.6"]
                                   [prismatic/schema "1.4.1"]
                                   [org.clojure/test.check "1.1.1"]
-                                  [org.clojure/tools.namespace "1.4.4"]
+                                  [org.clojure/tools.namespace "1.5.0"]
                                   [com.gfredericks/test.chuck "0.2.14"]
                                   ; com.bhauman/spell-spec library doesn't get any updates, so it has to be copied here
                                   ; under spec-tools.spell-spec namespace in order to fix its bugs.
                                   ; If the library gets updated with fixes it would be desirable to switch back to it.
                                   ;[com.bhauman/spell-spec "0.1.1"]
                                   [expound "0.9.0"]
-																																		[metosin/muuntaja "0.6.8"]
-                                  [metosin/ring-swagger "0.26.2"]
-                                  [metosin/jsonista "0.3.7"]
+																																		[metosin/muuntaja "0.6.10"]
+                                  [metosin/ring-swagger "1.0.0"]
+                                  [metosin/jsonista "0.3.8"]
                                   [metosin/scjsv "0.6.2"]]}
              :perf {:jvm-opts ^:replace ["-server"]}}
   :aliases {"all" ["with-profile" "dev"]
